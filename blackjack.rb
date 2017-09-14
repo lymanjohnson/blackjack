0dc0b32041e9b4a44ee46dc540763d4e16aa151d
lymanjohnson <lyman.johnson@gmail.com>
2017-09-14 17:47:50 -0400

    v0

--- blackjack.rb
+++ blackjack.rb
@@ -1,1 +1,15 @@
-puts "TODO Implement the game of blackjack."
+puts "Hello and welcome to the game of blackjack! Let's begin."
+
+def hit_again?
+  # loop until you get a good answer and return
+  while true
+    print "Do you want to (h)it or (s)tand? "
+    answer = gets.chomp.downcase
+    if answer[0] == "h" || "H"
+      return true
+    elsif answer[0] == "s" || "S"
+      return false
+    end
+    puts "That is not a valid answer!"
+  end
+end
