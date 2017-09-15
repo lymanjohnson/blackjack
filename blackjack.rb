d45ad8504af7565ed42c0dab452725687b179178
lymanjohnson <lyman.johnson@gmail.com>
2017-09-15 10:28:06 -0400

    input.rb working so far

--- blackjack.rb
+++ blackjack.rb
@@ -1,40 +1,15 @@
-puts "Thick cigarette smoke fills the room. It chokes your lungs and yet fails to hide the sharp odor of freon coming from above. The haze and gloom masks the faces of your fellow gamblers like two decades on a polaroid. You see some old familiars."
+# Dir["./lib/*.rb"].each {|file| require file}
 
+require './lib/input.rb'
+require './lib/card.rb'
+
+puts "Thick cigarette smoke fills the casino hall. It chokes your lungs and yet fails to hide the sharp odor of freon emanating from above. The haze and gloom masks the faces of your fellow gamblers like two decades on a polaroid. Still, you see some old familiars."
 
 
 game_on = true
 
 while game_on
 
-end
+  q_hit_again?
 
-
-
-
-def hit_again?
-  # loop until you get a good answer and return
-  while true
-    print "Do you want to (h)it or (s)tand? "
-    answer = gets.chomp.downcase
-    if answer[0] == "h" || "H"
-      return true
-    elsif answer[0] == "s" || "S"
-      return false
-    end
-    puts "That is not a valid answer!"
-  end
-end
-
-def new_player?
-  # loop until you get a good answer and return
-  while true
-    print "Do you want to (h)it or (s)tand? "
-    answer = gets.chomp.downcase
-    if answer[0] == "h" || "H"
-      return :smo
-    elsif answer[0] == "s" || "S"
-      return false
-    end
-    puts "That is not a valid answer!"
-  end
 end
--- lib/input.rb
+++ lib/input.rb
@@ -1,29 +1,11 @@
-def hit_again?
+def q_hit_again?
   # loop until you get a good answer and return
   while true
     print "Do you want to (h)it or (s)tand? "
     answer = gets.chomp.downcase
-    if answer[0] == "h" || "H"
+    if answer[0] == "h" || answer == "hit"
       return true
-    elsif answer[0] == "s" || "S"
-      return false
-    end
-    puts "That is not a valid answer!"
-  end
-end
-
-def new_player?
-  # loop until you get a good answer and return
-  while true
-    print "
-    [1]
-
-
-    "
-    answer = gets.chomp.downcase
-    if answer[0] == "h" || "H"
-      return :smo
-    elsif answer[0] == "s" || "S"
+    elsif answer[0] == "s" || answer == "stand"
       return false
     end
     puts "That is not a valid answer!"
